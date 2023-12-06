b8293347c7dcf7be1c68f1422c0a3bf0605cce34
MuhFadila <muh.fadila18@gmail.com>
2023-12-06 10:54:24 +0700

    create data class UIStateSiswa & DetailSiswa

--- app/src/main/java/com/example/roomsiswa/Model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/Model/EntryViewModel.kt
@@ -2,8 +2,23 @@ package com.example.roomsiswa.Model
 
 
 import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.Data.Siswa
 import com.example.roomsiswa.Repository.RepositorySiswa
 
 class EntryViewModel(private val repositorySiswa: RepositorySiswa): ViewModel() {
+
+
 }
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+
 
