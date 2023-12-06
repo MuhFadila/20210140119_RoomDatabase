e05058fa60da9251bca1ea3555601883f41e31de
MuhFadila <muh.fadila18@gmail.com>
2023-12-06 12:20:22 +0700

    add DestinasiEntry

--- app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanEntry.kt
@@ -10,6 +10,13 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomsiswa.Model.EntryViewModel
 import com.example.roomsiswa.Model.PenyediaViewModel
+import com.example.roomsiswa.Navigasi.DestinasiNavigasi
+import com.example.roomsiswa.Navigasi.SiswaTopAppBar
+import com.example.roomsiswa.R
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -22,5 +29,7 @@ fun EntrySiswaScreen(
     val coroutineScope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
+    }
 }
+
 
