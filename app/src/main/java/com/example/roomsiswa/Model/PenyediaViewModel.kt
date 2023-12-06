71e1d68b08ff40044d8585272bb8abbeb1c89db8
MuhFadila <muh.fadila18@gmail.com>
2023-12-06 11:30:15 +0700

    create class ContainerDataApp

--- app/src/main/java/com/example/roomsiswa/Model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/Model/PenyediaViewModel.kt
@@ -1,2 +1,4 @@
 package com.example.roomsiswa.Model
 
+
+
--- app/src/main/java/com/example/roomsiswa/Repository/ContainerApp.kt
+++ app/src/main/java/com/example/roomsiswa/Repository/ContainerApp.kt
@@ -1,2 +1,14 @@
 package com.example.roomsiswa.Repository
 
+import android.content.Context
+import com.example.roomsiswa.Data.DatabaseSiswa
+
+interface ContainerApp {
+    val repositorySiswa : RepositorySiswa
+}
+
+class ContainerDataApp(private val context: Context): ContainerApp{
+    override val repositorySiswa: RepositorySiswa by lazy{
+        OfflinerepositoriSiswa(DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
