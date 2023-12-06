9611bcc98f18d1e9ce9817315a89eb1450d14ebe
MuhFadila <muh.fadila18@gmail.com>
2023-12-06 12:26:05 +0700

    add 3 OutlinedTextField

--- app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanEntry.kt
@@ -2,9 +2,13 @@ package com.example.roomsiswa.ui.theme.Halaman
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
@@ -62,9 +66,42 @@ fun FormInputSiswa(
     onValueChange: (DetailSiswa) -> Unit = {},
     enabled: Boolean = true
 ) {
-
+    Column(
+        modifier = Modifier,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        OutlinedTextField(value = detailSiswa.nama,
+            onValueChange = {onValueChange(detailSiswa.copy(nama = it))},
+            label = { Text(stringResource(id = R.string.nama))},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(value = detailSiswa.alamat,
+            onValueChange = {onValueChange(detailSiswa.copy(alamat = it))},
+            label = { Text(stringResource(id = R.string.alamat))},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(value = detailSiswa.telpon,
+            onValueChange = {onValueChange(detailSiswa.copy(telpon = it))},
+            label = { Text(stringResource(id = R.string.telpon))},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        if (enabled){
+            Text(stringResource(id = R.string.required_field),
+                modifier = Modifier.padding(start = dimensionResource(id = R.dimen.padding_medium))
+            )
+        }
+        Divider(
+            thickness = dimensionResource(id = R.dimen.padding_small),
+            modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_medium))
+        )
     }
-
+}
 
 
 
