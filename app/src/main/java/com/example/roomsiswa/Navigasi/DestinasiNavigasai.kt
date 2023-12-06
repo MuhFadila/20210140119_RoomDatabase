188239f2f2c6555bd58f412b9ba4fd92a9939e58
MuhFadila <muh.fadila18@gmail.com>
2023-12-06 11:34:50 +0700

    create interface DestinasiNavigasi

--- app/src/main/java/com/example/roomsiswa/Navigasi/DestinasiNavigasai.kt
+++ app/src/main/java/com/example/roomsiswa/Navigasi/DestinasiNavigasai.kt
@@ -1,2 +1,13 @@
 package com.example.roomsiswa.Navigasi
 
+interface DestinasiNavigasi {
+    /**
+     * Nama unik untuk menentukan jalur untuk composable
+     */
+    val route: String
+
+    /**
+     * String resource id yang berisi judul akan ditampilkan dilayar halaman.
+     */
+    val titleRes: Int
+}
