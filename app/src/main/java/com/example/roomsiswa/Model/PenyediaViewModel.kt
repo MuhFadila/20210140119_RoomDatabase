b3112f7cac6baa3160d2cf030730036db45320ef
MuhFadila <muh.fadila18@gmail.com>
2023-12-06 11:32:48 +0700

    add object PenyediaViewModel

--- app/src/main/java/com/example/roomsiswa/Model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/Model/PenyediaViewModel.kt
@@ -1,4 +1,22 @@
 package com.example.roomsiswa.Model
 
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomsiswa.AplikasiSiswa
 
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+
+        initializer {
+            HomeViewModel(AplikasiSiswa().container.repositorySiswa)
+        }
+
+        initializer {
+            EntryViewModel(AplikasiSiswa().container.repositorySiswa)
+        }
+
+    }
+
+
+}
 
