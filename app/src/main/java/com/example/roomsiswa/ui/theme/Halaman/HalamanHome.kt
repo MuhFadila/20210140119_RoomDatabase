963b0caabcd0bea8d6ea42901c4ad030d49f40d0
MuhFadila <muh.fadila18@gmail.com>
2023-12-06 12:33:17 +0700

    create fun BodyHome

--- app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanHome.kt
@@ -1,10 +1,17 @@
 package com.example.roomsiswa.ui.theme.Halaman
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.roomsiswa.Data.Siswa
 import com.example.roomsiswa.Model.HomeViewModel
 import com.example.roomsiswa.Model.PenyediaViewModel
 import com.example.roomsiswa.Navigasi.DestinasiNavigasi
@@ -25,4 +32,25 @@ fun HomeScreen(
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
 
+}
+
+@Composable
+fun BodyHome(
+    itemSiswa: List<Siswa>,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        if (itemSiswa.isEmpty()) {
+            Text(
+                text = stringResource(id = R.string.deskripsi_no_item),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.titleLarge
+            )
+        } else {
+
+        }
+    }
 }
