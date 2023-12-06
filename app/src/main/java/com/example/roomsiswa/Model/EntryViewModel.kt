c20c7566d36f528de6a3c6cd946708656fa3e0c1
MuhFadila <muh.fadila18@gmail.com>
2023-12-06 10:56:42 +0700

    create var uiStateSiswa & fun validasiInput

--- app/src/main/java/com/example/roomsiswa/Model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/Model/EntryViewModel.kt
@@ -1,13 +1,37 @@
 package com.example.roomsiswa.Model
 
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.roomsiswa.Data.Siswa
 import com.example.roomsiswa.Repository.RepositorySiswa
 
 class EntryViewModel(private val repositorySiswa: RepositorySiswa): ViewModel() {
 
+    /*
+       *Berisi status Siswa saat ini
+        */
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
 
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+    fun updateUiState(detailSiswa: DetailSiswa){
+        uiStateSiswa = UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    /* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun saveSiswa(){
+        if (validasiInput()) {
+            repositorySiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
 }
 
 data class UIStateSiswa(
