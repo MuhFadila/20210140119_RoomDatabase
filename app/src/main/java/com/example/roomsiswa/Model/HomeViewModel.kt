7e177658c35fac87d7306827e175c39ee7bc0cd9
MuhFadila <muh.fadila18@gmail.com>
2023-12-06 10:57:54 +0700

    create class HomeViewModel

--- app/src/main/java/com/example/roomsiswa/Model/HomeViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/Model/HomeViewModel.kt
@@ -1,2 +1,9 @@
 package com.example.roomsiswa.Model
 
+import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.Repository.RepositorySiswa
+
+class HomeViewModel(private val repositorySiswa: RepositorySiswa) : ViewModel() {
+
+
+}
