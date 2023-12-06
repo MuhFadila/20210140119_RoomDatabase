77c8501d79e4daec33ea265c9feb4c6c745b4fb3
MuhFadila <muh.fadila18@gmail.com>
2023-12-06 10:59:08 +0700

    create data class HomeUiState

--- app/src/main/java/com/example/roomsiswa/Model/HomeViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/Model/HomeViewModel.kt
@@ -1,9 +1,15 @@
 package com.example.roomsiswa.Model
 
 import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.Data.Siswa
 import com.example.roomsiswa.Repository.RepositorySiswa
+import kotlinx.coroutines.flow.StateFlow
 
 class HomeViewModel(private val repositorySiswa: RepositorySiswa) : ViewModel() {
 
 
+
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
 }
