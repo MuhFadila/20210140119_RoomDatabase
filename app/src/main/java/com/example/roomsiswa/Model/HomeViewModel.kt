108192889b6488da70bdd608af5b2ca1414e953a
MuhFadila <muh.fadila18@gmail.com>
2023-12-06 11:02:34 +0700

    Add val homeUiState

--- app/src/main/java/com/example/roomsiswa/Model/HomeViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/Model/HomeViewModel.kt
@@ -1,14 +1,29 @@
 package com.example.roomsiswa.Model
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.roomsiswa.Data.Siswa
 import com.example.roomsiswa.Repository.RepositorySiswa
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class HomeViewModel(private val repositorySiswa: RepositorySiswa) : ViewModel() {
 
 
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
 
+    val homeUiState: StateFlow<HomeUiState> = repositorySiswa.getAllSiswa().filterNotNull()
+        .map { HomeUiState(listSiswa = it.toList()) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = HomeUiState()
+        )
     data class HomeUiState(
         val listSiswa: List<Siswa> = listOf()
     )
