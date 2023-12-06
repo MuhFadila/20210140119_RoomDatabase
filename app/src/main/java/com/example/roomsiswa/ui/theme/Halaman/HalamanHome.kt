c55733c2213955f24c168f64e0c16a81cef58661
MuhFadila <muh.fadila18@gmail.com>
2023-12-06 12:34:50 +0700

    create fun ListSiswa

--- app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanHome.kt
@@ -1,6 +1,8 @@
 package com.example.roomsiswa.ui.theme.Halaman
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -50,6 +52,17 @@ fun BodyHome(
                 style = MaterialTheme.typography.titleLarge
             )
         } else {
+
+        }
+    }
+}
+@Composable
+fun ListSiswa(
+    itemSiswa: List<Siswa>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier = Modifier) {
+        items(items = itemSiswa, key = { it.id }) { person ->
 
         }
     }
