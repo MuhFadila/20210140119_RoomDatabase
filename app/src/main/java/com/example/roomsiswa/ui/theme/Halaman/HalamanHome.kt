4efcc848dd16aea0d210e2177cffa68eb9bf22d9
MuhFadila <muh.fadila18@gmail.com>
2023-12-06 12:32:07 +0700

    create fun HomeScreen

--- app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanHome.kt
@@ -1,9 +1,28 @@
 package com.example.roomsiswa.ui.theme.Halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.roomsiswa.Model.HomeViewModel
+import com.example.roomsiswa.Model.PenyediaViewModel
 import com.example.roomsiswa.Navigasi.DestinasiNavigasi
 import com.example.roomsiswa.R
 
 object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes = R.string.app_name
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+
 }
