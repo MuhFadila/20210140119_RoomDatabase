a5a71ee3bbc2e2e9e1d01901c9eb9263320ef525
MuhFadila <muh.fadila18@gmail.com>
2023-12-06 12:24:10 +0700

    add fun EntrySiswaBody

--- app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanEntry.kt
@@ -1,5 +1,8 @@
 package com.example.roomsiswa.ui.theme.Halaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
@@ -7,9 +10,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.roomsiswa.Model.DetailSiswa
 import com.example.roomsiswa.Model.EntryViewModel
 import com.example.roomsiswa.Model.PenyediaViewModel
+import com.example.roomsiswa.Model.UIStateSiswa
 import com.example.roomsiswa.Navigasi.DestinasiNavigasi
 import com.example.roomsiswa.Navigasi.SiswaTopAppBar
 import com.example.roomsiswa.R
@@ -29,7 +36,24 @@ fun EntrySiswaScreen(
     val coroutineScope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
+
     }
 }
+
+@Composable
+fun EntrySiswaBody(
+    uiStateSiswa: UIStateSiswa,
+    onSiswaValueChange: (DetailSiswa) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large)),
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+
+    }
+}
+
 
 
