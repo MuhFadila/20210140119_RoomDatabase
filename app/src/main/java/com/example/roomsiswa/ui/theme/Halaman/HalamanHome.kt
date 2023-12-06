021f9551ac13523ec1b813e671c55ab9d7c27bb9
MuhFadila <muh.fadila18@gmail.com>
2023-12-06 12:31:06 +0700

    create object DestinasiHome

--- app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/Halaman/HalamanHome.kt
@@ -1,2 +1,9 @@
 package com.example.roomsiswa.ui.theme.Halaman
 
+import com.example.roomsiswa.Navigasi.DestinasiNavigasi
+import com.example.roomsiswa.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
